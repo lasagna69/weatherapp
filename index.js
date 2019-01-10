146c6f8bec5d6bdd0df0545fecb82e6e8d6a492a
Christian C. Barral <christian.barral@rococoglobaltech.com>
2019-01-10 10:28:54 +0800

    formatted JSON body received from openWeather

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ let request = require('request');
 
 let apiKey = '1e633184aa243831894d8d884c77daee';
 let city = 'Cebu city';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
 
 request(url, function (err, response, body) {
   if(err){
     console.log('error:', error);
   } else {
-    console.log('body:', body);
+  	let weather = JSON.parse(body);
+  	let message = `It is currently ${weather.main.temp} degrees in ${weather.name}`;
+    console.log(message);
   }
 });
