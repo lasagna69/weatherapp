9d9f1582f5673d4b2335391d58470f27e4efea5e
Christian C. Barral <christian.barral@rococoglobaltech.com>
2019-01-10 10:07:44 +0800

    Initial commit. index.js fetches weather for city

---
+++ index.js
@@ -0,0 +1,13 @@
+let request = require('request');
+
+let apiKey = '1e633184aa243831894d8d884c77daee';
+let city = 'cebu';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+
+request(url, function (err, response, body) {
+  if(err){
+    console.log('error:', error);
+  } else {
+    console.log('body:', body);
+  }
+});
