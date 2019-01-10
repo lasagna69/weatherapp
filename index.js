bd245b33820245dcca6a49fe29d1dbfa50501e30
Christian C. Barral <christian.barral@rococoglobaltech.com>
2019-01-10 10:21:10 +0800

    changed city to Cebu city

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let request = require('request');
 
 let apiKey = '1e633184aa243831894d8d884c77daee';
-let city = 'cebu';
+let city = 'Cebu city';
 let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
 
 request(url, function (err, response, body) {
